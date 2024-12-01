c3aaf1e27a53b7a9d2684ac21e9f71120d82075f
macbook <macbook@macbooks-MacBook-Pro.local>
2024-12-01 15:23:02 +0000

    update gitignore

--- App.tsx
+++ App.tsx
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import {
+  Button,
+  SafeAreaView,
+  StatusBar,
+  StyleProp,
+  useColorScheme,
+  ViewStyle,
+} from "react-native";
+import { Colors } from "react-native/Libraries/NewAppScreen";
 
-export default function App() {
+import { LiveActivityService } from "./src/services/live-activity";
+
+function App(): JSX.Element {
+  const isDarkMode = useColorScheme() === "dark";
+  const [activityTokenId, setActivityTokenId] = useState<string | null>(null);
+
+  const backgroundStyle: StyleProp<ViewStyle> = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 1,
+    width: "100%",
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? "light-content" : "dark-content"}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <Button
+        title="Activate live activity"
+        onPress={() =>
+          LiveActivityService.startLiveActivities(setActivityTokenId)
+        }
+      />
+      <Button
+        title="Update live activity"
+        onPress={() => LiveActivityService.updateLiveActivity(activityTokenId)}
+      />
+      <Button
+        title="Deactivate live activity"
+        onPress={LiveActivityService.endLiveActivities}
+      />
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
